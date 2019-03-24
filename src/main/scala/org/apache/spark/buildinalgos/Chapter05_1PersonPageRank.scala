867201a16f83b2aabf999ea69c671b0dca2388db
SamMa <sam_newyork@163.com>
2019-03-24 22:05:19 +0800

    graphx build-in algorithm page-rank

---
+++ src/main/scala/org/apache/spark/buildinalgos/Chapter05_1PersonPageRank.scala
@@ -0,0 +1,18 @@
+package org.apache.spark.buildinalgos
+
+import org.apache.spark.graphx.GraphLoader
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Chapter05_1PersonPageRank {
+
+  def main(args: Array[String]): Unit = {
+    val sparkContext = new SparkContext(new SparkConf().setMaster("local")
+      .setAppName("pageRank"))
+    val graph = GraphLoader.edgeListFile(sparkContext, "D:/datahub/spark-graphx/src/main/resources/Cit-HepTh.txt")
+    val graphOps = graph.personalizedPageRank(9207016, 0.001)
+        .vertices
+        .filter(_._1 != 9207016)
+        .reduce((a, b) => if (a._2 > b._2) a else b)
+    println(graphOps)
+  }
+}
