74fda5c20e21c4b670c4977b295b70c419a3b1fe
sam ma <sam_newyork@163.com>
2022-10-14 23:19:36 +0800

    add parallelism and partitions config in spark config

--- pyspark_api/app/car_lottery.py
+++ pyspark_api/app/car_lottery.py
@@ -7,4 +7,5 @@ if __name__ == '__main__':
 	spark: SparkSession = get_spark_context()
 	rootPath = "xxxx"
 	hdfs_path_apply = f"${rootPath}/apply"
+	# spark.default.parallelism和spark.sql.shuffle.partitions用来配置并行度，已经水了两次了，这样不好
 	spark.read.parquet(hdfs_path_apply)
