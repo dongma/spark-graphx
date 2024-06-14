32c6624c9569a2ef95256ceeadda2d579c809070
madong <madong@haizhi.com>
2024-06-15 01:34:42 +0800

    Dstream transformation, such as filter、count、reduceByKey、saveToJSon

--- spark-streaming/src/main/scala/lowapi/DTransformation.scala
+++ spark-streaming/src/main/scala/lowapi/DTransformation.scala
@@ -5,6 +5,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
+import java.io.File
 import java.sql.Date
 import java.time.{LocalDate, Period}
 
@@ -55,8 +56,35 @@ object DTransformation {
     List(people.firstName, people.lastName)
   }
 
+  def highIncomePeople() = readPeople().filter(_.salary > 80000)
+
+  def countPeople(): DStream[Long] = readPeople().count() // the number of entries in every batch
+
+  /*
+    reduce by key
+    - works on DStream of tuples
+    - works PER BATCH
+   */
+  def countNamesReduce(): DStream[(String, Int)] =
+    readPeople()
+      .map(_.firstName)
+      .map(name => (name, 1))
+      .reduceByKey((a, b) => a +b)
+
+  // for encoders to create Datasets
+  import spark.implicits._
+
+  def saveToJson() = readPeople().foreachRDD { rdd =>
+    val ds = spark.createDataset(rdd)
+    val f = new File("example-data/rtjvm/json")
+    val nFiles = f.listFiles().length
+    val path = s"example-data/rtjvm/json/$nFiles.json"
+
+    ds.write.json(path)
+  }
+
   def main(args: Array[String]): Unit = {
-    val stream = peopleSmallNames()
+    val stream = countNamesReduce()
     stream.print()
     ssc.start()
     ssc.awaitTermination()
---
+++ spark-streaming/src/main/scala/lowapi/WindowTransformation.scala
@@ -0,0 +1,15 @@
+package lowapi
+
+/**
+ * Window Transformation
+ *
+ * @author Sam Ma
+ * @date 2024/06/11
+ */
+object WindowTransformation {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
